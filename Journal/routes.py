1e28d584c6cea66c79d2f965076ad539d50d8ac5
Andra Hendisutio <andra.hendisutio@dattabot.io>
2017-08-13 17:36:29 +0700

    Third Commit

--- Journal/routes.py
+++ Journal/routes.py
@@ -5,7 +5,6 @@ from flask import render_template, g, make_response, request, Flask, redirect, u
 from flask.json import jsonify
 from flask_cors import CORS
 from Journal import app
-from contextlib import contextmanager
 
 import json
 import time
@@ -23,8 +22,6 @@ def after_request(response):
 
 @app.route('/')
 @app.route('/home')
-@contextmanager
 def home():
-	time.sleep(5)
-	if request:
-		return "This is a home page that you asked for !"
+	time.sleep(3)
+	return "This is a home page that you asked for !"
